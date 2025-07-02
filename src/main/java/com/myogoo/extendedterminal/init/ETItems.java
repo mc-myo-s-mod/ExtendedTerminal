36f6319be8b3b8c3c136f8023cd8555d516ec699
njoyshadow <shs3740413@gmail.com>
2025-07-02 17:58:01 +0900

    temp) add recipe & item

--- src/main/java/com/myogoo/extendedterminal/init/ETItems.java
+++ src/main/java/com/myogoo/extendedterminal/init/ETItems.java
@@ -15,12 +15,12 @@ public class ETItems {
 
     public static final List<ItemDefinition<?>> ITEMS = new ArrayList<>();
 
-    public static final ItemDefinition<Item> INTEGRATED_PROCESSOR = createItem("integrated processor",
-            ExtendedTerminal.makeId("integrated_processor"),
+    public static final ItemDefinition<Item> INTEGRATION_PROCESSOR = createItem("integrated processor",
+            ExtendedTerminal.makeId("integration_processor"),
             Item::new);
 
-    public static final ItemDefinition<?> PRINTED_INTEGRATED_CIRCUIT = createItem("printed integrated circuit",
-            ExtendedTerminal.makeId("printed_integrated_circuit"),
+    public static final ItemDefinition<?> PRINTED_INTEGRATION_PROCESSOR = createItem("printed integrated circuit",
+            ExtendedTerminal.makeId("printed_integration_processor"),
             Item::new);
 
     public static <T extends Item> ItemDefinition<T> createItem(String name, ResourceLocation id, Function<Item.Properties, T> itemFactory) {
