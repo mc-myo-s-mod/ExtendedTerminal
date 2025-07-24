b700b917141dc3f04623e66c2f431f2d7d16071c
njoyshadow <shs3740413@gmail.com>
2025-07-24 21:43:40 +0900

    porting 1.21.1-0.0.3

--- src/main/java/me/myogoo/extendedterminal/part/extendedcrafting/EliteTerminalPart.java
+++ src/main/java/me/myogoo/extendedterminal/part/extendedcrafting/EliteTerminalPart.java
@@ -5,6 +5,7 @@ import appeng.api.parts.IPartModel;
 import appeng.items.parts.PartModels;
 import appeng.parts.PartModel;
 import me.myogoo.extendedterminal.ExtendedTerminal;
+import me.myogoo.extendedterminal.config.ETConfig;
 import me.myogoo.extendedterminal.menu.ETMenuType;
 import me.myogoo.extendedterminal.menu.extendedcrafting.EliteTerminalMenu;
 import me.myogoo.extendedterminal.part.ETBaseTerminalPart;
@@ -21,7 +22,7 @@ public class EliteTerminalPart extends ETBaseTerminalPart {
     public static final IPartModel MODELS_HAS_CHANNEL = new PartModel(ELITE_MODEL_BASE, MODEL_ON, MODEL_STATUS_HAS_CHANNEL);
 
     public EliteTerminalPart(IPartItem<?> partItem) {
-        super(partItem, ETMenuType.ELITE_TERMINAL);
+        super(partItem, ETMenuType.ELITE_TERMINAL, ETConfig.ELITE_TERMINAL_CONFIG);
     }
 
     @Override
