8986f74fd08e9725d459ba0a3a2137644c8702bc
EvanHsieh0415 <evan940415@gmail.com>
2025-07-29 16:35:30 +0800

    refactor creative tab registration to use DeferredHolder

--- src/main/java/me/myogoo/extendedterminal/init/ETCreativeTab.java
+++ src/main/java/me/myogoo/extendedterminal/init/ETCreativeTab.java
@@ -6,22 +6,21 @@ import me.myogoo.extendedterminal.config.ETConfig;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ETCreativeTab {
     public static final DeferredRegister<CreativeModeTab> REGISTER = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ExtendedTerminal.MODID);
 
-    static {
-        REGISTER.register("itemgroup.extendedcrafting",() -> CreativeModeTab.builder()
-                .title(Component.translatable("itemgroup.extendedcrafting.title"))
-                .icon(ETParts.ULTIMATE_TERMINAL_PART::stack)
-                .displayItems((params, output) -> {
-                    for (ItemDefinition<?> item : ETItems.ITEMS) {
-                        if(!ETConfig.DISABLED_TERMINALS.getOrDefault(item,false)) {
-                            output.accept(item);
-                        }
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CREATIVE_TAB = REGISTER.register("creative_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup." + ExtendedTerminal.MODID))
+            .icon(ETParts.ULTIMATE_TERMINAL_PART::stack)
+            .displayItems((params, output) -> {
+                for (ItemDefinition<?> item : ETItems.ITEMS) {
+                    if (!ETConfig.DISABLED_TERMINALS.getOrDefault(item, false)) {
+                        output.accept(item);
                     }
-                })
-                .build());
-    }
+                }
+            })
+            .build());
 }
