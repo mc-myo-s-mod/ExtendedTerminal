513969939bad3a4b5b5b6de7e32dd402079a47d9
njoyshadow <shs3740413@gmail.com>
2025-08-03 21:45:42 +0900

    remove useless class

--- src/main/java/me/myogoo/extendedterminal/client/ETDataGen.java
+++ src/main/java/me/myogoo/extendedterminal/client/ETDataGen.java
@@ -1,7 +1,6 @@
 package me.myogoo.extendedterminal.client;
 
 import me.myogoo.extendedterminal.ExtendedTerminal;
-import me.myogoo.extendedterminal.client.provider.ExtendedCraftingRecipeProvider;
 import me.myogoo.extendedterminal.client.provider.InscriberRecipeProvider;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
