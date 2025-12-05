09b88c7fa32079fb2849ae67000b9e303190fe8b
njoyshadow <shs3740413@gmail.com>
2025-12-06 00:41:41 +0900

    add avaritiaNeo jei support

---
+++ src/main/java/me/myogoo/extendedterminal/integration/jei/avaritiaNeo/handler/AVNeoJeiRecipeTransferHandler.java
@@ -0,0 +1,108 @@
+package me.myogoo.extendedterminal.integration.jei.avaritiaNeo.handler;
+
+import appeng.core.localization.ItemModText;
+import me.myogoo.extendedterminal.api.adapter.recipe.IShapedTableRecipeAdapter;
+import me.myogoo.extendedterminal.api.adapter.recipe.ITableRecipeAdapter;
+import me.myogoo.extendedterminal.integration.jei.handler.AbstractTableRecipeHandler;
+import me.myogoo.extendedterminal.menu.avaritiaNeo.NeoExtremeTerminalMenu;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.transfer.IRecipeTransferError;
+import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
+import net.byAqua3.avaritia.loader.AvaritiaRecipes;
+import net.byAqua3.avaritia.recipe.RecipeExtremeCrafting;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.crafting.Ingredient;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static appeng.integration.modules.jeirei.TransferHelper.BLUE_PLUS_BUTTON_COLOR;
+import static appeng.integration.modules.jeirei.TransferHelper.ORANGE_PLUS_BUTTON_COLOR;
+import static me.myogoo.extendedterminal.integration.ItemListTermCraftingHelper.ensureFittedCraftingGrid;
+
+public class AVNeoJeiRecipeTransferHandler<T extends NeoExtremeTerminalMenu> extends AbstractTableRecipeHandler<T, RecipeExtremeCrafting> {
+    private final IRecipeTransferHandlerHelper helper;
+
+    public AVNeoJeiRecipeTransferHandler(Class<T> containerClass, MenuType<T> menuType, RecipeType<RecipeExtremeCrafting> recipeType, IRecipeTransferHandlerHelper helper) {
+        super(containerClass, menuType, recipeType);
+        this.helper = helper;
+    }
+
+    @Override
+    public @Nullable IRecipeTransferError transferRecipe(T menu, RecipeExtremeCrafting recipe, IRecipeSlotsView recipeSlots, Player player, boolean maxTransfer, boolean doTransfer) {
+        if (recipe.getIngredients().isEmpty()) {
+            return Result.createInCompatibleError(helper);
+        }
+
+        if (!recipe.canCraftInDimensions(menu.getCraftingGridWidth(), menu.getCraftingGridHeight())) {
+            return Result.createRecipeToLargeError(helper);
+        }
+
+        boolean craftMissing = AbstractContainerScreen.hasControlDown();
+        var inputSlots = recipeSlots.getSlotViews(RecipeIngredientRole.INPUT);
+        var adapterRecipe = ITableRecipeAdapter.of(recipe);
+
+        var slotToIngredientMap = getGuiSlotToIngredientMap(menu,adapterRecipe);
+        var missingSlots = menu.findMissingIngredients(slotToIngredientMap);
+
+        if (missingSlots.missingSlots().size() == slotToIngredientMap.size()) {
+            // All missing, can't do much...
+            var missingSlotViews = missingSlots.missingSlots().stream()
+                    .map(idx -> idx < inputSlots.size() ? inputSlots.get(idx) : null)
+                    .filter(Objects::nonNull)
+                    .toList();
+            return helper.createUserErrorForMissingSlots(ItemModText.NO_ITEMS.text(), missingSlotViews);
+        }
+
+        if (!doTransfer) {
+            if (missingSlots.totalSize() != 0) {
+                int color = missingSlots.anyMissing() ? ORANGE_PLUS_BUTTON_COLOR : BLUE_PLUS_BUTTON_COLOR;
+                return new Result.PartiallyCraftable(missingSlots, color, craftMissing);
+            }
+        } else {
+            performTransfer(menu, adapterRecipe, craftMissing);
+        }
+
+        return Result.createSuccessful();
+    }
+
+    @Override
+    public Map<Integer, Ingredient> getGuiSlotToIngredientMap(T menu, ITableRecipeAdapter recipe) {
+        int gridSideLength = menu.getCraftingGridWidth();
+        var raw = recipe.recipe().getIngredients();
+        List<Ingredient> ingredients;
+
+        int width = gridSideLength;
+        int height = gridSideLength;
+        if (recipe instanceof IShapedTableRecipeAdapter shapedRecipe) {
+            ingredients = ensureFittedCraftingGrid(shapedRecipe);
+            width = shapedRecipe.width();
+            height = shapedRecipe.height();
+        } else {
+            ingredients = raw;
+        }
+
+        int max = gridSideLength * gridSideLength;
+        int count = Math.min(ingredients.size(), max);
+        var result = new HashMap<Integer, Ingredient>(count);
+        for (int i = 0; i < count; i++) {
+            int x = i % width;
+            int y = i / width;
+
+            var guiSlot = y * gridSideLength + x;
+            var ing = ingredients.get(i);
+            if (!ing.isEmpty()) {
+                result.put(guiSlot, ing);
+            }
+        }
+        return result;
+    }
+
+}
