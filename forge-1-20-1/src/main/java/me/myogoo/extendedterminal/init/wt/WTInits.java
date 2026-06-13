142d45e76a8de763daa62f4ded16a7c3969b84b1
myogoo <shs3740413@gmail.com>
2026-06-14 00:47:31 +0900

    feat: support AE2 import export cards on wireless terminals

--- forge-1-20-1/src/main/java/me/myogoo/extendedterminal/init/wt/WTInits.java
+++ forge-1-20-1/src/main/java/me/myogoo/extendedterminal/init/wt/WTInits.java
@@ -1,6 +1,7 @@
 package me.myogoo.extendedterminal.init.wt;
 
 import appeng.api.upgrades.Upgrades;
+import net.minecraft.core.registries.BuiltInRegistries;
 import de.mari_023.ae2wtlib.AE2wtlib;
 import me.myogoo.extendedterminal.me.host.ETWTHost;
 import me.myogoo.extendedterminal.me.host.UnitedWTHost;
@@ -8,6 +9,11 @@ import me.myogoo.extendedterminal.menu.ETMenuType;
 import me.myogoo.extendedterminal.menu.extendedcrafting.wt.UnitedWTMenu;
 import me.myogoo.extendedterminal.menu.extendedterminal.wt.ETWTMenu;
 import me.myogoo.myotus.api.wt.AddTerminalEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.ItemLike;
+
+import java.util.Optional;
 
 public final class WTInits {
     private WTInits() {
@@ -33,6 +39,20 @@ public final class WTInits {
                     "item.extendedterminal.wireless_united_terminal");
             Upgrades.add(AE2wtlib.MAGNET_CARD, WTItems.WIRELESS_ET_TERMINAL.asItem(), 1);
             Upgrades.add(AE2wtlib.MAGNET_CARD, WTItems.WIRELESS_UNITED_TERMINAL.asItem(), 1);
+            registerImportExportCards(WTItems.WIRELESS_ET_TERMINAL.asItem());
+            registerImportExportCards(WTItems.WIRELESS_UNITED_TERMINAL.asItem());
         });
+    }
+
+    private static void registerImportExportCards(ItemLike terminal) {
+        findImportExportCard("import_card").ifPresent(card -> Upgrades.add(card, terminal, 1));
+        findImportExportCard("export_card").ifPresent(card -> Upgrades.add(card, terminal, 1));
+    }
+
+    private static Optional<Item> findImportExportCard(String path) {
+        var modernId = new ResourceLocation("ae2importexportcard", path);
+        var legacyId = new ResourceLocation("ae2insertexportcard", path);
+        return BuiltInRegistries.ITEM.getOptional(modernId)
+                .or(() -> BuiltInRegistries.ITEM.getOptional(legacyId));
     }
 }
--- neoforge-1-21-1/src/main/java/me/myogoo/extendedterminal/init/wt/WTInit.java
+++ neoforge-1-21-1/src/main/java/me/myogoo/extendedterminal/init/wt/WTInit.java
@@ -21,11 +21,14 @@ import me.myogoo.myotus.api.MyotusAPI;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.registries.RegisterEvent;
+
+import java.util.Optional;
 
 import static me.myogoo.extendedterminal.init.wt.WTItems.WT_ITEMS;
 
@@ -62,9 +65,23 @@ public class WTInit {
         Upgrades.add(AE2wtlibItems.QUANTUM_BRIDGE_CARD, WTItems.WIRELESS_ET_TERMINAL.asItem(), 1);
         Upgrades.add(AE2wtlibItems.MAGNET_CARD, WTItems.WIRELESS_UNITED_TERMINAL.asItem(), 1);
         Upgrades.add(AE2wtlibItems.QUANTUM_BRIDGE_CARD, WTItems.WIRELESS_UNITED_TERMINAL.asItem(), 1);
+        registerImportExportCards(WTItems.WIRELESS_ET_TERMINAL.asItem());
+        registerImportExportCards(WTItems.WIRELESS_UNITED_TERMINAL.asItem());
 
         GridLinkables.register(WTItems.WIRELESS_ET_TERMINAL.asItem(), WirelessTerminalItem.LINKABLE_HANDLER);
         GridLinkables.register(WTItems.WIRELESS_UNITED_TERMINAL.asItem(), WirelessTerminalItem.LINKABLE_HANDLER);
+    }
+
+    private static void registerImportExportCards(ItemLike terminal) {
+        findImportExportCard("import_card").ifPresent(card -> Upgrades.add(card, terminal, 1));
+        findImportExportCard("export_card").ifPresent(card -> Upgrades.add(card, terminal, 1));
+    }
+
+    private static Optional<Item> findImportExportCard(String path) {
+        var modernId = ResourceLocation.fromNamespaceAndPath("ae2importexportcard", path);
+        var legacyId = ResourceLocation.fromNamespaceAndPath("ae2insertexportcard", path);
+        return BuiltInRegistries.ITEM.getOptional(modernId)
+                .or(() -> BuiltInRegistries.ITEM.getOptional(legacyId));
     }
 
     public static void initCapabilities(RegisterCapabilitiesEvent event) {
